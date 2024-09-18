44832d9c867d694b17e84ec66ee94b6126d37cdc
Elsie Oduor <eoatieno@gmail.com>
2024-09-18 14:04:55 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({super.key});
+
+  String _currentFortune = "";
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _currentFortune = "";
+
+  final _fortuneList = [
+    "You will find a new friend",
+    "You will find a lover tomorrow",
+    "You will find a job tomorrow",
+    "You will find a better tomorrow",
+    "A cow will cross your path",
+    "Monkeys will rule the Earth",
+    "You are being played",
+    "You love coding"
+  ];
+
+  void _randomFortune() {
+    var random = Random();
+    int fortune = random.nextInt(_fortuneList.length);
+    setState(() {
+      _currentFortune = _fortuneList[fortune];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Flutter Demo Fortune Page"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // Text(
+            //   'Your fortune is:',
+            //   style: TextStyle(
+            //     fontSize: 19,
+            //     fontWeight: FontWeight.bold),
+            // ),
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(
+                  '${_currentFortune}',
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+              ),
+            ),
+            ElevatedButton(
+                onPressed: _randomFortune, child: Text("Get Fortune")),
+          ],
+        ),
+      ),
+    );
+  }
+}
